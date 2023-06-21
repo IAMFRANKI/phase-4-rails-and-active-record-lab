e378ad5294b14d5c42e6ad4656f7d8e1fa9cc41f
frank <thmsfrank04@gmail.com>
2023-06-20 20:40:13 -0700

    Complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-    create_table "students", force: :cascade do |t|
-        t.string "first_name"
-        t.string "last_name"
-        t.integer "grade"
-        t.datetime "created_at", precision: 6, null: false
-        t.datetime "updated_at", precision: 6, null: false
-      end
+ActiveRecord::Schema.define(version: 2023_06_21_032301) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "grade"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
